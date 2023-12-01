0f92f82f1a7542ef7e9688a991e6c6019b94c6ca
Yu-lianka <146694164+Yu-lianka@users.noreply.github.com>
2023-12-01 14:24:07 +0300

    Update bird_main.py
    Теперь выводит птичку на экран

--- bird_main.py
+++ bird_main.py
@@ -11,26 +11,51 @@ DBLUE=(25,25,112)
 RED=(255,83,73)
 WHITE=(230,230,250)
 GAME_COLORS=[YELLOW,BLUE,PURPLE,DBLUE,RED]
+SPEED=15
+
+SCALE=100
 
 WIDTH = 800
 HEIGHT = 800
 
+
+
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("bird_simulator")
+s=0
+
 
 finished = False
 
 clock = pygame.time.Clock()
+bird=Bird(screen)
+
 
 while not finished:
     screen.fill(WHITE)
+    bird.draw()
+    pygame.display.update()
 
-    pygame.display.flip()
+    if s==1:
+        bird.move()
+
+    
 
     clock.tick(FPS)
+    
+    keys = pygame.key.get_pressed()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+
+        elif keys[pygame.K_SPACE] and s==0:
+            bird.start()
+            s+=1
+        elif keys[pygame.K_SPACE]:
+            bird.start()
+            
 
 pygame.quit()
