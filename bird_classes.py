4cb549ef08a9f3df6a3411b03aab6ac16f9eebc4
Yu-lianka <146694164+Yu-lianka@users.noreply.github.com>
2023-12-01 14:17:37 +0300

    Update bird_classes.py
    class bird changed: добавлены функции start, draw; изменена функция move

--- bird_classes.py
+++ bird_classes.py
@@ -7,31 +7,65 @@ img_berry = pygame.image.load('berry.png')
 img_berry.convert()
 rect = img_berry.get_rect()
 
-img_bird = pygame.image.load('bird.png')
+'''img_bird = pygame.image.load('bird.png')
 img_bird.convert()
-rect = img_bird.get_rect()
+rect = img_bird.get_rect()'''
 
 class Bird:
-    def __init__(self, screen: pygame.Surface, x=WIDTH*0.5, y=HEIGTH*0.5):
+    def __init__(self, screen: pygame.Surface, x=WIDTH*0.5, y=HEIGHT*0.5):
+        '''Создание птички, загрузка изображения'''
         self.screen = screen
         self.x = x
         self.y = y
         self.vx = 0
         self.vy = 0
+        self.ind=1
+        self.f=0
 
+        self.BIRD = pygame.image.load('BIRD_1.png').convert_alpha()
+        self.BIRD=pygame.transform.scale(self.BIRD,(SCALE*1.3,SCALE))
 
-    def move(self):
-        if self.x > WIDTH:
-            self.vx = self.vx * (-1)
-        if self.x < 0:
-            self.vx = self.vx * (-1)
+    def start(self):
+        '''Прыжок: вызывается нажатием пробела или при столкновении со стеной'''
+        self.vy=-SPEED-2
+        self.vx=SPEED*self.ind
         self.x += self.vx
         self.y += self.vy
+        
+    def move(self):
+        '''Перемещение птички, отталкивание от стен'''
+        g=2
+        self.vx=SPEED*self.ind
+        self.y += self.vy
+        self.x += self.vx
+        self.vy += g
+        
+        if self.x + SCALE*1.3*0.5 > WIDTH-10:
+            self.ind *=(-1)
+            self.vy = -SPEED
+            self.x-=10
+            self.f=1
+            
+        if self.x - SCALE*1.3*0.5 < 0+10:
+            self.ind *=(-1)
+            self.vy = -SPEED
+            self.x+=10
+            self.f=1
+
     def hittest(self, obj):
+        '''Проверка сталкивания с шипами. ЭТУ ФУНКЦИЮ НЕ ТРОГАЛА'''
         if (self.x-obj.x)**2+ (self.y-obj.y)**2<(self.r+obj.r)**2:
             return True
         else:
             return False
+
+    def draw(self):
+        '''Вывод птички на экран + отражение при изменении направления движения'''
+        if self.f==1:
+            self.BIRD=pygame.transform.flip(self.BIRD,1,0)
+            self.f=0
+            
+        screen.blit(self.BIRD,[self.x-SCALE*1.3*0.5,self.y-SCALE*0.5])
  
  
 class Ship:
