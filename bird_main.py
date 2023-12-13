a1609a7ec07c8a4048a5cde9c1c2599f2375fb1b
Yu-lianka <146694164+Yu-lianka@users.noreply.github.com>
2023-12-13 11:20:07 +0300

    Update bird_main.py
    Добавлен счётчик баллов; есть опция выбора персонажа; + темнеет экран при ударе о шип

--- bird_main.py
+++ bird_main.py
@@ -6,19 +6,23 @@ import sys
 from bird_classes import *
 
 FPS = 30
-YELLOW=(255,168,18)
-BLUE=(24,255,209)
-PURPLE=(116,66,200)
-DBLUE=(25,25,112)
-RED=(255,83,73)
-WHITE=(230,230,250)
-GAME_COLORS=[YELLOW,BLUE,PURPLE,DBLUE,RED]
-SPEED=15
-
-SCALE=100
+MyGREEN=(58,107,56)
+DRED=(127,24,13)
+RED = 0xFF0000
+BLUE = 0x0000FF
+YELLOW = 0xFFC91F
+GREEN = 0x00FF00
+MAGENTA = 0xFF03B8
+CYAN = 0x00FFCC
+BLACK = (0, 0, 0)
+WHITE = 0xFFFFFF
+GREY = 0x7D7D7D
+GAME_COLORS = [YELLOW, GREEN, MAGENTA, CYAN]
 
 WIDTH = 800
-HEIGHT = 800
+HEIGHT = 600
+POINTS = 0
+LIFE=5
 
 
 
@@ -26,22 +30,115 @@ HEIGHT = 800
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("bird_simulator")
+
+BIRD_1 = pygame.image.load('BIRD_1.png').convert_alpha()
+BIRD_1=pygame.transform.scale(BIRD_1,(SCALE*1.3,SCALE))
+
+BIRD_2 = pygame.image.load('BIRD_2.png').convert_alpha()
+BIRD_2 =pygame.transform.scale(BIRD_2,(SCALE*1.3,SCALE))
+BIRD_2=pygame.transform.flip(BIRD_2,1,0)
+
+chosen = False
+
+BACK=WHITE
+
+
+
+
+
 s=0
 spikes=[]
 spimove=[]
+bottom=[]
 r = 1
 m=0
 
 finished = False
-
+chosen = False
 clock = pygame.time.Clock()
-bird=Bird(screen)
 
+screen.fill(WHITE)
+pygame.draw.line(screen, DBLUE, [0, 0], [0, HEIGHT], 32)
+pygame.draw.line(screen, DBLUE, [WIDTH, 0], [WIDTH, HEIGHT], 32)
 
+#Выбор персонажа
+while not chosen:
+    pygame.display.flip()
+    Mouse_x, Mouse_y = pygame.mouse.get_pos()
+    if Mouse_x>75 and Mouse_x<325 and Mouse_y>275 and Mouse_y<525:
+        mycol1=YELLOW
+        mycol2=BLUE
+    elif Mouse_x>475 and Mouse_x<725 and Mouse_y>275 and Mouse_y<550:
+        mycol1=BLUE
+        mycol2=PURPLE
+    else:
+        mycol1=mycol2=BLUE
+    for X in range(48,816,64):
+        spd=Spike(screen, X, 64)
+        spd.draw_down(HEIGHT-spd.a/(2*math.sqrt(3)))
+        spu=Spike(screen, X, -64)
+        spu.draw_down(abs(spu.a)/(2*math.sqrt(3)))
+        bottom.append(spd)
+        bottom.append(spu)
+        
+    pygame.draw.circle(screen, (mycol1), (200, 400), 150)
+    pygame.draw.circle(screen, (mycol2), (600, 400), 150)
+    pygame.draw.circle(screen, (BLUE), (200, 400), 170, 10)
+    pygame.draw.circle(screen, (BLUE), (600, 400), 170, 10)
+    screen.blit(BIRD_1,[200-SCALE*1.3*0.5,400-SCALE*0.5])
+    screen.blit(BIRD_2,[600-SCALE*1.3*0.5,400-SCALE*0.5])
+    
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if Mouse_x<400:
+                SCALE=100
+                BIRD_1=pygame.transform.scale(BIRD_1,(SCALE*1.3,SCALE))
+                BIRD=BIRD_1
+                chosen = True
+            elif Mouse_x>400:
+                SCALE=100
+                BIRD_2 =pygame.transform.scale(BIRD_2,(SCALE*1.3,SCALE))
+                BIRD=BIRD_2
+                chosen = True
+bird=Bird(screen, BIRD)
                 
 while not finished:
-    screen.fill(WHITE)
+    
+    screen.fill(BACK)
+    
+
+
+            
+
+    if chosen == True:
+        pygame.draw.circle(screen, (BLUE), (WIDTH*0.5, HEIGHT*0.5), 280, 16)
+        pygame.draw.circle(screen, (BLUE), (WIDTH*0.5, HEIGHT*0.5), 250)
+
+    
+    text = ' {score:n}'
+    font = pygame.font.SysFont(None,550)
+
+    img=font.render(text.format(score=s),True,BACK)
+    pos = img.get_rect(center=(WIDTH//2-59, HEIGHT//2+10))
+
+    screen.blit(img,pos)
+
+
+    
     bird.draw()
+
+    for X in range(48,816,64):
+        spd=Spike(screen, X, 64)
+        spd.draw_down(HEIGHT-spd.a/(2*math.sqrt(3)))
+        spu=Spike(screen, X, -64)
+        spu.draw_down(abs(spu.a)/(2*math.sqrt(3)))
+        bottom.append(spd)
+        bottom.append(spu)
+    
+    pygame.draw.line(screen, DBLUE, [0, 0], [0, HEIGHT], 32)
+    pygame.draw.line(screen, DBLUE, [WIDTH, 0], [WIDTH, HEIGHT], 32)
+
+
 
     if bird.r==1 and r==1 :
        
@@ -51,11 +148,20 @@ while not finished:
             n=s
         else:
             n=rnd.randint(1,5)
+            z=rnd.randint(0,1)
         for i in range (n):
-            spike=Spike(screen,-5,-80)
-            spikes.append(spike)
-        s+=1
+            z=rnd.randint(0,1)
+            spike=Spike(screen, -100+32,-75)
+            if z==0 or s<=5:
+                spikes.append(spike)
+            elif z==1 and s>5:
+                spimove.append(spike)
+        for j in range (m):
+            spike=Spike(screen, -100+32,-75)
+            spimove.append(spike)
         r=0
+        s+=1
+
     elif bird.r==0 and r==0 and s>=1:
         spikes=[]
         spimove=[]
@@ -66,13 +172,13 @@ while not finished:
             z=rnd.randint(0,1)
         for i in range (n):
             z=rnd.randint(0,1)
-            spike=Spike(screen,805,80)
+            spike=Spike(screen, WIDTH+5+100,75)
             if z==0 or s<=5:
                 spikes.append(spike)
             elif z==1 and s>5:
                 spimove.append(spike)
         for j in range (m):
-            spike=Spike(screen,805,80)
+            spike=Spike(screen, WIDTH+5+100,75)
             spimove.append(spike)
         r=1
         s+=1
@@ -80,6 +186,7 @@ while not finished:
     for i in spikes:
         i.draw()
         i.appear()
+    
     for j in spimove:
         j.draw()
         j.appear()
@@ -116,6 +223,9 @@ while not finished:
             SPEED=0
             bird.vx=0
             bird.vy=30
+            BACK=GREY
+
+            #bird.hit
 
     for j in spimove:
         if bird.hittest(j):
@@ -123,6 +233,17 @@ while not finished:
             SPEED=0
             bird.vx=0
             bird.vy=30
+            BACK=GREY
+
+            #bird.hit()
+
+    if bird.hit_bottom():
+        bird.live=0
+        SPEED=0
+        bird.vx=0
+        bird.vy=30
+        BACK=GREY
+
 
     if bird.live==0 and bird.y>800:
         s=0
@@ -132,7 +253,8 @@ while not finished:
         bird.x=WIDTH*0.5
         bird.y=HEIGHT*0.5
         bird.draw()
+        BACK=WHITE
+
         SPEED=15
         
 pygame.quit()
-
