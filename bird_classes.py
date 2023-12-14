a28ce6addb18b38438e20d6410b4e80637041006
Nikita Payusov <npayusov1@gmail.com>
2023-12-15 00:14:07 +0300

    FIX Pep8

--- bird_classes.py
+++ bird_classes.py
@@ -1,13 +1,13 @@
 import math
 import random as rnd
 import pygame
-import time
 
 from bird_const import *
 
+
 class Bird:
-    def __init__(self, screen: pygame.Surface, bird, x=WIDTH*0.5, y=HEIGHT*0.5):
-        '''Создание птички, загрузка изображения'''
+    def __init__(self, screen: pygame.Surface, bird, x=WIDTH * 0.5, y=HEIGHT * 0.5):
+        """Создание птички, загрузка изображения"""
         print(HEIGHT, WIDTH)
         self.screen = screen
         self.x = x
@@ -15,130 +15,125 @@ class Bird:
         self.vx = 0
         self.vy = 0
         self.ind = 1
-        self.f=0
-        self.live=1
-        self.r=0
-        self.R=SCALE*1.3*0.25
-        self.BIRD=bird
+        self.f = 0
+        self.live = 1
+        self.r = 0
+        self.R = SCALE * 1.3 * 0.25
+        self.BIRD = bird
 
     def start(self):
-        '''Прыжок: вызывается нажатием пробела или при столкновении со стеной'''
-        if self.live==1:
-            self.vy=-SPEED-2
-            self.vx=SPEED*self.ind
+        """Прыжок: вызывается нажатием пробела или при столкновении со стеной"""
+        if self.live == 1:
+            self.vy = -SPEED - 2
+            self.vx = SPEED * self.ind
             self.x += self.vx
             self.y += self.vy
-        
+
     def move(self):
-        '''Перемещение птички, отталкивание от стен'''
-        g=2
-        self.vx=SPEED*self.ind
+        """Перемещение птички, отталкивание от стен"""
+        g = 2
+        self.vx = SPEED * self.ind
         self.y += self.vy
         self.x += self.vx
         self.vy += g
-        
-        if self.x + SCALE*1.3*0.25 > WIDTH-10 and self.live==1:
-            self.ind *=(-1)
+
+        if self.x + SCALE * 1.3 * 0.25 > WIDTH - 10 and self.live == 1:
+            self.ind *= (-1)
             self.vy = -SPEED
-            self.x-=10
-            self.f=1
-            self.r=1
-           
-            
-        if self.x - SCALE*1.3*0.25 < 0+10 and self.live==1:
-            self.ind *=(-1)
+            self.x -= 10
+            self.f = 1
+            self.r = 1
+
+        if self.x - SCALE * 1.3 * 0.25 < 0 + 10 and self.live == 1:
+            self.ind *= (-1)
             self.vy = -SPEED
-            self.x+=10
-            self.f=1
-            self.r=0
+            self.x += 10
+            self.f = 1
+            self.r = 0
 
     def hittest(self, obj):
-        '''Проверка сталкивания с шипами на стенах'''
+        """Проверка сталкивания с шипами на стенах"""
 
-        return ((obj.x + obj.a/(2*math.sqrt(3))+60 > self.x-self.R and \
-           obj.x + obj.a/(2*math.sqrt(3)) < self.x+self.R and \
-           obj.y+1 > self.y-SCALE*0.25 and \
-           obj.y-1 < self.y+SCALE*0.25 and obj.a<0) or \
-           (obj.x - obj.a/(2*math.sqrt(3))+60 > self.x-self.R and \
-           obj.x - obj.a/(2*math.sqrt(3)) < self.x+self.R and \
-           obj.y+1 > self.y-SCALE*0.25 and \
-           obj.y-1 < self.y+SCALE*0.25 and obj.a>0))
+        return ((obj.x + obj.a / (2 * math.sqrt(3)) + 60 > self.x - self.R and 
+                 obj.x + obj.a / (2 * math.sqrt(3)) < self.x + self.R and 
+                 obj.y + 1 > self.y - SCALE * 0.25 and 
+                 obj.y - 1 < self.y + SCALE * 0.25 and obj.a < 0) or 
+                (obj.x - obj.a / (2 * math.sqrt(3)) + 60 > self.x - self.R and 
+                 obj.x - obj.a / (2 * math.sqrt(3)) < self.x + self.R and 
+                 obj.y + 1 > self.y - SCALE * 0.25 and 
+                 obj.y - 1 < self.y + SCALE * 0.25 and obj.a > 0))
 
-    def hit_bottom (self):
-        return (self.y+SCALE*0.25>HEIGHT-40 or self.y-SCALE*0.25<40)
+    def hit_bottom(self):
+        return self.y + SCALE * 0.25 > HEIGHT - 40 or self.y - SCALE * 0.25 < 40
 
     def draw(self):
-        '''Вывод птички на экран + отражение при изменении направления движения'''
-        if self.f==1:
-            self.BIRD=pygame.transform.flip(self.BIRD,1,0)
-            self.f=0
-            
-        self.screen.blit(self.BIRD,[self.x-SCALE*1.3*0.25,self.y-SCALE*0.25])
+        """Вывод птички на экран + отражение при изменении направления движения"""
+        if self.f == 1:
+            self.BIRD = pygame.transform.flip(self.BIRD, 1, 0)
+            self.f = 0
 
-        
+        self.screen.blit(self.BIRD, [self.x - SCALE * 1.3 * 0.25, self.y - SCALE * 0.25])
+
 
 class Spike:
-    def __init__(self, screen: pygame.Surface, x,a):
-        '''Иниацилизация шипов'''
+    def __init__(self, screen: pygame.Surface, x, a):
+        """Иниацилизация шипов"""
         self.screen = screen
         self.x = x
-        self.y =rnd.randint(64,HEIGHT-64)
+        self.y = rnd.randint(64, HEIGHT - 64)
         self.a = a
         self.points = 0
         self.live = 1
         self.color = DBLUE
-        self.vy=5*rnd.uniform(-1,1)
+        self.vy = 5 * rnd.uniform(-1, 1)
 
-        
     def new_spike(self, x):
-        self.y=rnd.randint(64,HEIGHT-64)
-        self.x=x
+        self.y = rnd.randint(64, HEIGHT - 64)
+        self.x = x
 
     def move(self):
-        '''Движение шипов по вертикали '''
-        
-        self.y+=self.vy
+        """Движение шипов по вертикали """
 
+        self.y += self.vy
 
-        if self.y+self.a/2>HEIGHT-5:
-            self.vy*=-1
-            self.y-=5
+        if self.y + self.a / 2 > HEIGHT - 5:
+            self.vy *= -1
+            self.y -= 5
 
-        elif self.y - abs(self.a)/2<5:
-            self.vy*=-1
-            self.y+=5
+        elif self.y - abs(self.a) / 2 < 5:
+            self.vy *= -1
+            self.y += 5
 
     def appear(self):
-        '''Появление шипов (выползание из-за стены) '''
-        if self.a<0:
-            if self.x<26:
-                self.x+=10
+        """Появление шипов (выползание из-за стены) """
+        if self.a < 0:
+            if self.x < 26:
+                self.x += 10
         else:
-            if self.x>790-24:
-                self.x-=10
+            if self.x > 790 - 24:
+                self.x -= 10
 
-
-    def draw_down(self,y):
-        self.y=y
+    def draw_down(self, y):
+        self.y = y
         pygame.draw.polygon(
             self.screen,
             self.color,
-            [(self.x - self.a/2, self.y + self.a/(2*math.sqrt(3))),
-             (self.x, self.y - self.a/math.sqrt(3)),
-             (self.x + self.a/2, self.y + self.a/(2*math.sqrt(3))) ]
+            [(self.x - self.a / 2, self.y + self.a / (2 * math.sqrt(3))),
+             (self.x, self.y - self.a / math.sqrt(3)),
+             (self.x + self.a / 2, self.y + self.a / (2 * math.sqrt(3)))]
         )
 
     def draw(self):
-        '''Прорисовка шипов'''
+        """Прорисовка шипов"""
 
         pygame.draw.polygon(
             self.screen,
             self.color,
-            [(self.x + self.a/(2*math.sqrt(3)), self.y - self.a/2),
-             (self.x - 2 * self.a/(2*math.sqrt(3)), self.y),
-             (self.x + self.a/(2*math.sqrt(3)), self.y + self.a/2 ) ]
+            [(self.x + self.a / (2 * math.sqrt(3)), self.y - self.a / 2),
+             (self.x - 2 * self.a / (2 * math.sqrt(3)), self.y),
+             (self.x + self.a / (2 * math.sqrt(3)), self.y + self.a / 2)]
         )
-        
+
     def hit(self, points=1):
         """Попадание птички в шип."""
         self.points += points
@@ -146,24 +141,20 @@ class Spike:
 
 class Berry:
     def __init__(self, screen: pygame.Surface):
-        '''Создание птички, загрузка изображения'''
+        """Создание птички, загрузка изображения"""
         self.screen = screen
-        self.x = x = rnd(30, 770)
-        self.y = y = rnd(30, 770)
+        self.x = x = rnd.randint(30, 770)
+        self.y = y = rnd.randint(30, 770)
         self.live = 1
-        self.f=0
+        self.f = 0
         self.new_berry()
 
     def new_berry(self):
         """ Инициализация новой. """
         self.live = 1
-        x = self.x = rnd(30, 770)
-        y = self.y = rnd(30, 770)
+        x = self.x = rnd.randint(30, 770)
+        y = self.y = rnd.randint(30, 770)
 
     def hit(self, points=1):
         """Съедание ягоды."""
         self.points += points
-
-
-
-
--- bird_main.py
+++ bird_main.py
@@ -1,33 +1,28 @@
-import math
-import random as rnd
-import pygame
-import sys
 from bird_classes import *
 from bird_const import *
+
 
 def main():
     global SPEED, SCALE
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
     pygame.display.set_caption("bird_simulator")
 
-    BIRD_1 = pygame.image.load('pictures/BIRD_1.png').convert_alpha()
-    BIRD_1=pygame.transform.scale(BIRD_1,(SCALE*1.3,SCALE))
+    bird_1 = pygame.image.load('pictures/BIRD_1.png').convert_alpha()
+    bird_1 = pygame.transform.scale(bird_1, (SCALE * 1.3, SCALE))
 
-    BIRD_2 = pygame.image.load('pictures/BIRD_2.png').convert_alpha()
-    BIRD_2 =pygame.transform.scale(BIRD_2,(SCALE*1.3,SCALE))
-    BIRD_2=pygame.transform.flip(BIRD_2,1,0)
+    bird_2 = pygame.image.load('pictures/BIRD_2.png').convert_alpha()
+    bird_2 = pygame.transform.scale(bird_2, (SCALE * 1.3, SCALE))
+    bird_2 = pygame.transform.flip(bird_2, 1, 0)
 
-    chosen = False
-    BACK=WHITE
-    s=0
-    spikes=[]
-    spimove=[]
-    bottom=[]
+    back = WHITE
+    s = 0
+    spikes = []
+    spimove = []
+    bottom = []
     r = 1
-    m=0
+    m = 0
 
     finished = False
     chosen = False
@@ -39,192 +34,182 @@ def main():
 
     while not chosen:
         pygame.display.flip()
-        Mouse_x, Mouse_y = pygame.mouse.get_pos()
-        if Mouse_x>75 and Mouse_x<325 and Mouse_y>275 and Mouse_y<525:
-            mycol1=YELLOW
-            mycol2=BLUE
-        elif Mouse_x>475 and Mouse_x<725 and Mouse_y>275 and Mouse_y<550:
-            mycol1=BLUE
-            mycol2=PURPLE
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if 75 < mouse_x < 325 and 275 < mouse_y < 525:
+            mycol1 = YELLOW
+            mycol2 = BLUE
+        elif 475 < mouse_x < 725 and 275 < mouse_y < 550:
+            mycol1 = BLUE
+            mycol2 = PURPLE
         else:
-            mycol1=mycol2=BLUE
-        for X in range(48,816,64):
-            spd=Spike(screen, X, 64)
-            spd.draw_down(HEIGHT-spd.a/(2*math.sqrt(3)))
-            spu=Spike(screen, X, -64)
-            spu.draw_down(abs(spu.a)/(2*math.sqrt(3)))
+            mycol1 = mycol2 = BLUE
+        for X in range(48, 816, 64):
+            spd = Spike(screen, X, 64)
+            spd.draw_down(HEIGHT - spd.a / (2 * math.sqrt(3)))
+            spu = Spike(screen, X, -64)
+            spu.draw_down(abs(spu.a) / (2 * math.sqrt(3)))
             bottom.append(spd)
             bottom.append(spu)
-            
-        pygame.draw.circle(screen, (mycol1), (200, 400), 150)
-        pygame.draw.circle(screen, (mycol2), (600, 400), 150)
-        pygame.draw.circle(screen, (BLUE), (200, 400), 170, 10)
-        pygame.draw.circle(screen, (BLUE), (600, 400), 170, 10)
-        screen.blit(BIRD_1,[200-SCALE*1.3*0.5,400-SCALE*0.5])
-        screen.blit(BIRD_2,[600-SCALE*1.3*0.5,400-SCALE*0.5])
-        
+
+        pygame.draw.circle(screen, mycol1, (200, 400), 150)
+        pygame.draw.circle(screen, mycol2, (600, 400), 150)
+        pygame.draw.circle(screen, BLUE, (200, 400), 170, 10)
+        pygame.draw.circle(screen, BLUE, (600, 400), 170, 10)
+        screen.blit(bird_1, [200 - SCALE * 1.3 * 0.5, 400 - SCALE * 0.5])
+        screen.blit(bird_2, [600 - SCALE * 1.3 * 0.5, 400 - SCALE * 0.5])
+
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if Mouse_x<400:
-                    SCALE=100
-                    BIRD_1=pygame.transform.scale(BIRD_1,(SCALE*1.3,SCALE))
-                    BIRD=BIRD_1
+                if mouse_x < 400:
+                    SCALE = 100
+                    bird_1 = pygame.transform.scale(bird_1, (SCALE * 1.3, SCALE))
+                    birdx = bird_1
                     chosen = True
-                elif Mouse_x>400:
-                    SCALE=100
-                    BIRD_2 =pygame.transform.scale(BIRD_2,(SCALE*1.3,SCALE))
-                    BIRD=BIRD_2
+                elif mouse_x > 400:
+                    SCALE = 100
+                    bird_2 = pygame.transform.scale(bird_2, (SCALE * 1.3, SCALE))
+                    birdx = bird_2
                     chosen = True
-    bird=Bird(screen, BIRD)
-                    
+    bird = Bird(screen, birdx)
+
     while not finished:
-        screen.fill(BACK)
+        screen.fill(back)
 
-        if chosen == True:
-            pygame.draw.circle(screen, (BLUE), (WIDTH*0.5, HEIGHT*0.5), 280, 16)
-            pygame.draw.circle(screen, (BLUE), (WIDTH*0.5, HEIGHT*0.5), 250)
+        if chosen:
+            pygame.draw.circle(screen, BLUE, (WIDTH * 0.5, HEIGHT * 0.5), 280, 16)
+            pygame.draw.circle(screen, BLUE, (WIDTH * 0.5, HEIGHT * 0.5), 250)
 
-        
         text = ' {score:n}'
-        font = pygame.font.SysFont(None,550)
+        font = pygame.font.SysFont(None, 550)
 
-        img=font.render(text.format(score=s),True,BACK)
-        pos = img.get_rect(center=(WIDTH//2-59, HEIGHT//2+10))
+        img = font.render(text.format(score=s), True, back)
+        pos = img.get_rect(center=(WIDTH // 2 - 59, HEIGHT // 2 + 10))
 
-        screen.blit(img,pos)
+        screen.blit(img, pos)
 
         bird.draw()
 
-        for X in range(48,816,64):
-            spd=Spike(screen, X, 64)
-            spd.draw_down(HEIGHT-spd.a/(2*math.sqrt(3)))
-            spu=Spike(screen, X, -64)
-            spu.draw_down(abs(spu.a)/(2*math.sqrt(3)))
+        for X in range(48, 816, 64):
+            spd = Spike(screen, X, 64)
+            spd.draw_down(HEIGHT - spd.a / (2 * math.sqrt(3)))
+            spu = Spike(screen, X, -64)
+            spu.draw_down(abs(spu.a) / (2 * math.sqrt(3)))
             bottom.append(spd)
             bottom.append(spu)
-        
+
         pygame.draw.line(screen, DBLUE, [0, 0], [0, HEIGHT], 32)
         pygame.draw.line(screen, DBLUE, [WIDTH, 0], [WIDTH, HEIGHT], 32)
 
+        if bird.r == 1 and r == 1:
 
-
-        if bird.r==1 and r==1 :
-           
-            spikes=[]
-            spimove=[]
-            if s<=5:
-                n=s
+            spikes = []
+            spimove = []
+            if s <= 5:
+                n = s
             else:
-                n=rnd.randint(1,5)
-                z=rnd.randint(0,1)
-            for i in range (n):
-                z=rnd.randint(0,1)
-                spike=Spike(screen, -100+32,-75)
-                if z==0 or s<=5:
+                n = rnd.randint(1, 5)
+            for i in range(n):
+                z = rnd.randint(0, 1)
+                spike = Spike(screen, -100 + 32, -75)
+                if z == 0 or s <= 5:
                     spikes.append(spike)
-                elif z==1 and s>5:
+                elif z == 1 and s > 5:
                     spimove.append(spike)
-            for j in range (m):
-                spike=Spike(screen, -100+32,-75)
+            for j in range(m):
+                spike = Spike(screen, -100 + 32, -75)
                 spimove.append(spike)
-            r=0
-            s+=1
+            r = 0
+            s += 1
 
-        elif bird.r==0 and r==0 and s>=1:
-            spikes=[]
-            spimove=[]
-            if s<=5:
-                n=s
+        elif bird.r == 0 and r == 0 and s >= 1:
+            spikes = []
+            spimove = []
+            if s <= 5:
+                n = s
             else:
-                n=rnd.randint(1,5)
-                z=rnd.randint(0,1)
-            for i in range (n):
-                z=rnd.randint(0,1)
-                spike=Spike(screen, WIDTH+5+100,75)
-                if z==0 or s<=5:
+                n = rnd.randint(1, 5)
+            for i in range(n):
+                z = rnd.randint(0, 1)
+                spike = Spike(screen, WIDTH + 5 + 100, 75)
+                if z == 0 or s <= 5:
                     spikes.append(spike)
-                elif z==1 and s>5:
+                elif z == 1 and s > 5:
                     spimove.append(spike)
-            for j in range (m):
-                spike=Spike(screen, WIDTH+5+100,75)
+            for j in range(m):
+                spike = Spike(screen, WIDTH + 5 + 100, 75)
                 spimove.append(spike)
-            r=1
-            s+=1
-                
+            r = 1
+            s += 1
+
         for i in spikes:
             i.draw()
             i.appear()
-        
+
         for j in spimove:
             j.draw()
             j.appear()
             j.move()
-        
+
         pygame.display.update()
 
-
-        if s>0:
+        if s > 0:
             bird.move()
-        
-            
-        
 
         clock.tick(FPS)
-        
+
         keys = pygame.key.get_pressed()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
 
-            elif keys[pygame.K_SPACE] and s==0:
+            elif keys[pygame.K_SPACE] and s == 0:
 
                 bird.start()
-                s+=1
+                s += 1
 
             elif keys[pygame.K_SPACE]:
                 bird.start()
 
         for i in spikes:
             if bird.hittest(i):
-                bird.live=0
-                SPEED=0
-                bird.vx=0
-                bird.vy=30
-                BACK=GREY
+                bird.live = 0
+                SPEED = 0
+                bird.vx = 0
+                bird.vy = 30
+                back = GREY
 
-                #bird.hit
+                # bird.hit
 
         for j in spimove:
             if bird.hittest(j):
-                bird.live=0
-                SPEED=0
-                bird.vx=0
-                bird.vy=30
-                BACK=GREY
+                bird.live = 0
+                SPEED = 0
+                bird.vx = 0
+                bird.vy = 30
+                back = GREY
 
-                #bird.hit()
+                # bird.hit()
 
         if bird.hit_bottom():
-            bird.live=0
-            SPEED=0
-            bird.vx=0
-            bird.vy=30
-            BACK=GREY
+            bird.live = 0
+            SPEED = 0
+            bird.vx = 0
+            bird.vy = 30
+            back = GREY
 
-
-        if bird.live==0 and bird.y>800:
-            s=0
-            spikes=[]
-            spimove=[]
-            bird.live=1
-            bird.x=WIDTH*0.5
-            bird.y=HEIGHT*0.5
+        if bird.live == 0 and bird.y > 800:
+            s = 0
+            spikes = []
+            spimove = []
+            bird.live = 1
+            bird.x = WIDTH * 0.5
+            bird.y = HEIGHT * 0.5
             bird.draw()
-            BACK=WHITE
+            back = WHITE
 
-            SPEED=15
-            
+            SPEED = 15
+
     pygame.quit()
 
 
