6f088f5223e94781746225c292649e6993fe4aac
Yu-lianka <146694164+Yu-lianka@users.noreply.github.com>
2023-12-14 23:01:25 +0300

    Update bird_classes.py
    constants are imported from another file

--- bird_classes.py
+++ bird_classes.py
@@ -3,36 +3,11 @@ import random as rnd
 import pygame
 import time
 
-
-FPS = 30
-YELLOW=(255,168,18)
-BLUE=(178,182,235)
-PURPLE=(116,66,200)
-DBLUE=(25,25,112)
-RED=(255,83,73)
-WHITE=(230,230,250)
-MYBLUE=(226,224,249)
-
-DGREY=(66,60,99)
-GREY=(162,162,208)
-
-GAME_COLORS=[YELLOW,BLUE,PURPLE,DBLUE,RED]
-SPEED=15
-
-SCALE=200
-
-WIDTH = 800
-HEIGHT = 800
-
-
+import bird_const *
 
 img_berry = pygame.image.load('berry.png')
 img_berry.convert()
 rect = img_berry.get_rect()
-
-'''img_bird = pygame.image.load('bird.png')
-img_bird.convert()
-rect = img_bird.get_rect()'''
 
 class Bird:
     def __init__(self, screen: pygame.Surface, bird, x=WIDTH*0.5, y=HEIGHT*0.5):
@@ -42,15 +17,12 @@ class Bird:
         self.y = y
         self.vx = 0
         self.vy = 0
-        self.ind=1
+        self.ind = 1
         self.f=0
         self.live=1
         self.r=0
         self.R=SCALE*1.3*0.5
         self.BIRD=bird
-
-
-
 
     def start(self):
         '''Прыжок: вызывается нажатием пробела или при столкновении со стеной'''
@@ -86,28 +58,17 @@ class Bird:
     def hittest(self, obj):
         '''Проверка сталкивания с шипами на стенах'''
 
-        if (obj.x + obj.a/(2*math.sqrt(3))+60 > self.x-self.R and \
+        return ((obj.x + obj.a/(2*math.sqrt(3))+60 > self.x-self.R and \
            obj.x + obj.a/(2*math.sqrt(3)) < self.x+self.R and \
            obj.y+1 > self.y-SCALE*0.5 and \
            obj.y-1 < self.y+SCALE*0.5 and obj.a<0) or \
            (obj.x - obj.a/(2*math.sqrt(3))+60 > self.x-self.R and \
            obj.x - obj.a/(2*math.sqrt(3)) < self.x+self.R and \
            obj.y+1 > self.y-SCALE*0.5 and \
-           obj.y-1 < self.y+SCALE*0.5 and obj.a>0):
-            return True
-        else:
-            return False
+           obj.y-1 < self.y+SCALE*0.5 and obj.a>0))
 
     def hit_bottom (self):
-        '''if (obj.y-obj.a/math.sqrt(3)<self.y+SCALE*0.5 and obj.a>0) or \
-        (obj.y-obj.a/math.sqrt(3)>self.y-SCALE*0.5 and obj.a<0):'''
-        if self.y+SCALE*0.5>HEIGHT-40 or self.y-SCALE*0.5<40:
-            return True
-        else:
-            return False
-
-    #def hit (self):
-        
+        return (self.y+SCALE*0.5>HEIGHT-40 or self.y-SCALE*0.5<40)
 
     def draw(self):
         '''Вывод птички на экран + отражение при изменении направления движения'''
@@ -115,8 +76,11 @@ class Bird:
             self.BIRD=pygame.transform.flip(self.BIRD,1,0)
             self.f=0
             
-        screen.blit(self.BIRD,[self.x-SCALE*1.3*0.5,self.y-SCALE*0.5])
- 
+        self.screen.blit(self.BIRD,[self.x-SCALE*1.3*0.5,self.y-SCALE*0.5])
+
+
+        
+
 class Spike:
     def __init__(self, screen: pygame.Surface, x,a):
         '''Иниацилизация шипов'''
@@ -168,26 +132,21 @@ class Spike:
              (self.x + self.a/2, self.y + self.a/(2*math.sqrt(3))) ]
         )
 
-
-
     def draw(self):
         '''Прорисовка шипов'''
 
         pygame.draw.polygon(
-            screen,
+            self.screen,
             self.color,
             [(self.x + self.a/(2*math.sqrt(3)), self.y - self.a/2),
              (self.x - 2 * self.a/(2*math.sqrt(3)), self.y),
              (self.x + self.a/(2*math.sqrt(3)), self.y + self.a/2 ) ]
         )
-
         
-        
-                   
-
     def hit(self, points=1):
         """Попадание птички в шип."""
         self.points += points
+
 
 class Berry:
     def __init__(self, screen: pygame.Surface):
